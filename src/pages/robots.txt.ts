95dff8ed06939a6ecc20ad5cd8caa6553f2971d5
ItsHarryB <github@h.brightonandco.co.uk>
2026-08-22 16:20:19 +0100

    fixed visual shifting with sort and filter on shop page, v5.2.5

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = ({ site }) => {
-  const siteUrl = site?.toString() || 'https://example.com';
+  const siteUrl = site?.toString() || 'https://brightonandco.co.uk';
 
   const robotsTxt = `
 User-agent: *
